e3d98ecd655eb79b808f88dbc9c6026bb6d527a2
adamkujawski <a.kujawski90@gmail.com>
2022-05-12 10:51:31 +0200

    Added errors handling.

--- index.ts
+++ index.ts
@@ -1,16 +1,25 @@
 import express = require("express");
 import cors = require("cors");
 import 'express-async-errors'
-import { json } from "express";
+import {json} from "express";
+import {handleError} from "./utils/errors";
 
 const app = express();
 
 app.use(cors({
-    origin:'http://localhost:3000'
+    origin: 'http://localhost:3000'
 }));
 
 app.use(json());
 
-app.listen(3001,'0.0.0.0',()=>{
+
+// Routes
+// Sprawdzenie czy walidacja bledow dziala
+// app.get('/', async (req,res)=> {
+//     throw new Error('Damn');
+// })
+app.use(handleError)
+
+app.listen(3001, '0.0.0.0', () => {
     console.log("Server litening on http://localhost:3001")
 })
