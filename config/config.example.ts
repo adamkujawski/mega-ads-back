13a49854da92f23f39db4039635ec83c9b5706d2
adamkujawski <a.kujawski90@gmail.com>
2022-05-21 22:52:11 +0200

    Added config example and move config database to config.

---
+++ config/config.example.ts
@@ -0,0 +1,7 @@
+// export const config = {
+//     dbHost: 'localhost',
+//     dbUser: 'user',
+//     dbPort: 2020,
+//     dbPassword: 'password',
+//     dbDatabase: 'database',
+// }
--- utils/db.ts
+++ utils/db.ts
@@ -1,11 +1,12 @@
 import {createPool} from "mysql2/promise";
+import {config} from "../config/config";
 
 export const pool = createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    port: 3306,
-    password: '',
-    database: 'megak_ads',
+    host: config.dbHost,
+    user: config.dbUser,
+    port: config.dbPort,
+    password: config.dbPassword,
+    database: config.dbDatabase,
     namedPlaceholders: true, // pozwala na bezpieczne wysylanie danych
     decimalNumbers: true, // aby liczby byly liczbami
 })
