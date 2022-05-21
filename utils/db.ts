f05f28eff29ebabb9c9b9a95a5bee4438a9f307b
adamkujawski <a.kujawski90@gmail.com>
2022-05-21 23:06:51 +0200

    Added config example and move config database to config.

--- utils/db.ts
+++ utils/db.ts
@@ -4,7 +4,6 @@ import {config} from "../config/config";
 export const pool = createPool({
     host: config.dbHost,
     user: config.dbUser,
-    port: config.dbPort,
     password: config.dbPassword,
     database: config.dbDatabase,
     namedPlaceholders: true, // pozwala na bezpieczne wysylanie danych
